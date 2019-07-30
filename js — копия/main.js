e64fbb8b03e73e114b70d4bbeee45a3048ef809f
Nik68rus <Nik200x@mail.ru>
2019-07-30 23:41:00 +0300

    удалил случайно созданную папку

--- js — копия/main.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-(function () {
-  window.init.disactivate();
-  window.form.initForm();
-  window.mainPin.renderAddress(window.mainPin.getMainPinCoords(window.constants.MainPinSize.RADIUS));
-})();
